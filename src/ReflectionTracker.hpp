ea5f4e8308f5d4b64a7187715457e6f934080b28
Simon Peter <speter-toshiba@SP-Portege.(none)>
2013-07-01 12:06:15 -0500

    Added functions to find and return reflections for a single region. Also organized the code and solved a bug with certain returned overlapping matches.

--- src/ReflectionTracker.hpp
+++ src/ReflectionTracker.hpp
@@ -4,13 +4,16 @@
 #include </usr/include/opencv2/opencv.hpp>
 #include </usr/include/opencv/highgui.h>
 #include <opencv2/highgui/highgui_c.h>
+#include "ARC_Pair.hpp"
+
 void createTemplatesFromVector(Mat image, int patchSize, vector<Point> *points, vector<Mat> *templates, vector<ARC_Pair> *outvector);
 Point findBestMatchLocation(Mat image, Mat sourceTemplate, int TLCornerTemplate);
 void findReflections(Mat image, int patchSize, vector<Point> *points, vector<Mat> *templates, vector<Rect> *reflections, vector<ARC_Pair> *outvector);
 void runSymmetryTest(Mat frame, int patchSize, vector<Point> *points, vector<Rect> *reflections, vector<Rect> *originalMatches, vector<ARC_Pair> *outvector);
 void identifyRealObjects(vector<ARC_Pair> *outvector);
-int getReflections(Mat frame, int patchSize, vector<ARC_Pair> &outvector);
+int getReflections(Mat frame, int patchSize, int numOfFeatures, vector<ARC_Pair> &outvector);
 void displayReflectionMatches(Mat image, int patchSize, vector<ARC_Pair> *outvector);
 Rect findOneReflection(Mat source, Mat tmplte, int leftBound);
 Rect findOneReflection(Mat source, Rect tmplte);
+ARC_Pair getOneReflectionPair(Mat image, int patchSize, bool *regionFound);
 #endif /*REFLECTION_TRACKER_H*/
